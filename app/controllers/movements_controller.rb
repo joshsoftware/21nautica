bdc168eaacf405b1a7237c27b51d77cac6799a2c
apsc92 <anurag@joshsoftware.com>
2014-07-31 11:24:28 +0530

    refs #5 Done some changes in Movement Controller

---
+++ app/controllers/movements_controller.rb
@@ -0,0 +1,20 @@
+class MovementsController < ApplicationController
+  def index
+  end
+  def create
+  	 movement = Movement.new(movement_params)
+    if movement.save
+      flash[:notice] = 'Successfully added new Movement'
+    else
+      flash[:error] = 'Error while saving movement'
+    end
+    @movement = Movement.all.to_a
+    render 'new'
+  end
+  def new
+  	@movement=Movement.new
+  end
+  def movement_params
+    params.require(:movement).permit(:booking_number, :truck_number,:vessel_targeted,:point_of_destination,:movement_type)
+  end
+end
