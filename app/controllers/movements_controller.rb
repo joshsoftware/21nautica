d2143b8754214000c8dad202c5cc07b81724fe6d
Lakshmi-Sharma <lakshmi@joshsoftware.com>
2014-07-31 18:54:05 +0530

    Refs #3 Improved Movement table

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,13 +1,7 @@
 class MovementsController < ApplicationController
 
   def index
-    @movements = Movement.where.not(status: "document_handed")
-    @booking_number_list = @movements.select(:booking_number).uniq.order(booking_number: :asc)
-    @movements_list = @booking_number_list.collect do |b_n| 
-      @movements.where(booking_number: b_n.booking_number).select do |mov| 
-        !mov.export_item.as_json.nil?
-      end
-    end
+    @movements = Movement.where.not(status: "document_handed").group_by(&:booking_number)
   end
 
   def create
