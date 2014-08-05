73ecf6fb0d5c3a73a2aa6ff9293f0aadfcbf0319
Gautam Rege <gautam@joshsoftware.com>
2014-08-05 18:39:46 +0530

    Fixing CircleCI with maintain_test_schema!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,7 @@ DatabaseCleaner.strategy = :transaction
 # require "minitest/pride"
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  ActiveRecord::Migration.maintain_test_schema!
 
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
