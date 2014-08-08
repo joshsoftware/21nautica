81320536731d6adf8bc1399d18109ae081df65e1
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-08-08 17:18:33 +0530

    refs #12 added empty containers worksheet

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: ENV['SENDGRID_USERNAME']
-  def mail_report(user)
-  	@user = user
+  def mail_report(customer)
+  	@customer = customer
     time = DateTime.parse(Time.now.to_s).strftime("%d_%b_%Y")
     daily_report =Report::Daily.new
-    daily_report.create(@user)
-	attachments["#{user.name.tr(" ", "_")}_#{time}.xlsx"] = File.read("#{Rails.root}/tmp/#{user.name.tr(" ", "_")}_#{time}.xlsx")
-    mail(to: @user.emails, subject: 'DAILY REPORT')
+    daily_report.create(@customer)
+	attachments["#{customer.name.tr(" ", "_")}_#{time}.xlsx"] = File.read("#{Rails.root}/tmp/#{customer.name.tr(" ", "_")}_#{time}.xlsx")
+    mail(to: @customer.emails, subject: 'DAILY REPORT')
   end
 end
