52a99de9f9625b00e1bb2c3944bba6bdf900d2f9
Lakshmi-Sharma <lakshmi@joshsoftware.com>
2014-08-08 12:36:41 +0530

    Refs #11 Cleaner code for Update Date

--- app/helpers/movements_helper.rb
+++ app/helpers/movements_helper.rb
@@ -1,2 +1,9 @@
 module MovementsHelper
+	def status_updated_at(element)
+		audit = element.audits.select do |k| 
+			k[:audited_changes][:status].present? && 
+			k[:audited_changes][:status].second == element.status 
+		end
+		audit.first.present? ? audit.first.audited_changes[:updated_at].second : nil 
+	end
 end
