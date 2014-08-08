74c061553dd2fc96717a60311ad2cfe2f2c8d9cc
Gautam Rege <gautam@joshsoftware.com>
2014-08-08 20:39:36 +0530

    refs #17 formatting change
    + indentation in movement_helper
    + changed date to short date

--- app/helpers/movements_helper.rb
+++ app/helpers/movements_helper.rb
@@ -1,19 +1,19 @@
 module MovementsHelper
-	def status_updated_at(element)
-		audit = element.audits.select do |k| 
-			k[:audited_changes][:status].present? && 
-			k[:audited_changes][:status].second == element.status 
-		end
-		audit.first.present? ? audit.first.audited_changes[:updated_at].second : nil 
-	end
+  def status_updated_at(element)
+    audit = element.audits.select do |k| 
+      k[:audited_changes][:status].present? && 
+      k[:audited_changes][:status].second == element.status 
+    end
+    audit.first.present? ? audit.first.audited_changes[:updated_at].second : nil 
+  end
 
-	def time_in_seconds(time)
-		seconds = time[0]*24*60*60 + time[1]*60*60 + time[2]*60 + time[3]
-	end
+  def time_in_seconds(time)
+    seconds = time[0]*24*60*60 + time[1]*60*60 + time[2]*60 + time[3]
+  end
 
-	def alert(updated_at, element)
+  def alert(updated_at, element)
     time = time_in_seconds(STATUS_CHANGE_DURATION[element.aasm.events.first.to_sym])
     (Time.now - updated_at) > time  
-	end
+  end
 
 end
