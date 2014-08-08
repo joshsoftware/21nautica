7754daed38dc7425b451c74da53f7ca0ea230879
apsc92 <anurag@joshsoftware.com>
2014-08-08 13:35:36 +0530

    Refs #21 Added dropdown box for Movement popup

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,5 @@
 DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
-SHIPPING_LINE =%W(CMA\ CGM Maersk  Evergreen Safmarine PIL)
+SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL)
+TRANSPORTERS = %W(1 2)
