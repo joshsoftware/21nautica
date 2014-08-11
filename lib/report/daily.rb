895b6dbb8359c84a146d4e457895039626d13867
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-08-11 13:00:06 +0530

    refs #12 solved issues caused due to new states and modified empty containers worksheet

--- lib/report/daily.rb
+++ lib/report/daily.rb
@@ -8,43 +8,42 @@ module Report
       exports = customer.exports.includes(:export_items)
 
       workbook.styles do |s|
-        heading = s.add_style alignment: {horizontal: :center}, 
+        heading = s.add_style alignment: {horizontal: :center},
           b: true, sz: 12, bg_color: "4F81BD", wrap_text: true,
           :border => {:style => :thin, :color => "00"}
-        center = s.add_style alignment: {horizontal: :center}, 
+        center = s.add_style alignment: {horizontal: :center},
           :border => {:style => :thin, :color => "00" }
         column_style = s.add_style alignment: {horizontal: :center}, bg_color: "E6E0EC",
           :border => {:style => :thin, :color => "00" }
 
         # 2 worksheets in the workbook
-        { 'Under Clearnance' => nil, 'HANDED OVER' => 'document_handed',
+        { 'Under Clearnance' => nil, 'HANDED OVER' => 'container_handed_over_to_KPA',
       'empty cntrs' => 'empty_containers'}.each do |name, status|
           workbook.add_worksheet(name: name) do |sheet|
-            sheet.add_row ["Truck No", "Booking No", "Vessal Targeted", "POL", "POD",
-                     "Container", "Type", "weight", "shipping Line","shipping seal",
-                     "customer seal","Loaded date","Arrived at Malaba Border",
-                     "Crossed Malaba Border","Order Release Date","Arrived Port date",
-                     "Document Handed Date", "Current Status"],
-                     style: heading, height: 40
             if !status.eql?('empty_containers') then
-              add_data(exports, sheet, center, status)
+              add_data(exports, sheet, center, heading, status)
             else
-              add_empty_cntr_data(exports, sheet, center)
+              add_empty_cntr_data(exports, sheet, heading, center)
             end
             sheet.col_style 2, column_style,  row_offset: 1
             sheet.col_style 5, column_style,  row_offset: 1
           end
         end
-      end 
+      end
       package.use_shared_strings = true
 
       package.serialize("#{Rails.root}/tmp/#{customer.name.tr(" ", "_")}_#{time}.xlsx")
-                        #filename: "#{customer.name.tr(" ", "_")}_#{time}.xlsx", 
-                          #type: "application/vnd.ms-excel")
 
     end
 
-    def add_data(exports, sheet, center, status)
+    def add_data(exports, sheet, center, heading, status)
+      sheet.add_row ["Truck No", "Booking No", "Vessal Targeted", "POL", "POD",
+                  "Container", "Type", "weight", "shipping Line","shipping seal",
+                  "customer seal","Loaded date","Arrived at Malaba Border",
+                  "Crossed Malaba Border","Order Release Date","Arrived Port date",
+                  "Document Handed Date", "Current Status"],
+                  style: heading, height: 40
+
       exports.each do |export|
         export.export_items.each do |item|
           movements = Movement.where(id: item.movement_id)
@@ -60,12 +59,14 @@ module Report
             end
 
             sheet.add_row [movement.truck_number, movement.booking_number,
-                     movement.vessel_targeted, movement.port_of_discharge, 
-                     movement.port_of_loading, item.container, export.equipment,
+                     movement.vessel_targeted, movement.port_of_loading,
+                     movement.port_of_discharge, item.container, export.equipment,
                      item.weight, export.shipping_line, movement.shipping_seal,
-                     movement.custom_seal, h["loaded"], h["arrived_malaba_border"], 
-                     h["crossed_malaba_border"], h["order_released"], h["arrived_port"],
-                     h["document_handed"], movement.current_location], style: center
+                     movement.custom_seal, h["loaded"], h["under_customs_clearance"],
+                     h["enroute_mombasa"],
+                     h["arranging_shipping_order_and_vessel_nomination"],
+                     h["arrived_port"],h["container_handed_over_to_KPA"],
+                     movement.remarks], style: center
 
             h.clear
           end
@@ -73,14 +74,19 @@ module Report
       end
     end
 
-    def add_empty_cntr_data(exports, sheet, center)
+    def add_empty_cntr_data(exports, sheet, heading,center)
+      sheet.add_row ["Date Of Placement", "Release Order Number",
+                  "Container Number", "Location", "shipping Line",
+                  "Number Of Days since Placed"], style: heading, height: 40
+
       exports.each do |export|
         export_items = export.export_items.where(movement_id: nil)
         export_items.each do |item|
-          sheet.add_row [nil, nil, nil, nil, nil,
-                    item.container.nil? ? "TBL" : item.container, export.equipment,
-                    item.weight, export.shipping_line, nil, nil, nil, nil, nil, nil,
-                    nil, nil], style: center
+          sheet.add_row [item.date_of_placement.to_date.strftime("%d-%b-%Y"),
+                    export.release_order_number,
+                    item.container.nil? ? "TBA" : item.container,
+                    item.location, export.shipping_line,
+                    (DateTime.now- item.date_of_placement).to_i],style: center
         end
       end
     end
