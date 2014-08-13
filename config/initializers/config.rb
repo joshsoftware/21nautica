a101de5d0075940a989a40d98b169823716a47f9
Gautam Rege <gautam@joshsoftware.com>
2014-08-13 22:45:02 +0530

    Added transporters

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,7 +2,7 @@ DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
 SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL)
-TRANSPORTERS = %W(1 2)
+TRANSPORTERS = %W(Mansons Farsham Panafrica Crown)
 # Time in Days
 STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           crossed_malaba_border: 2, 
