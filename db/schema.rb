03c03fdcc85432b9780ba95875b9428b755f4af2
apsc92 <anurag@joshsoftware.com>
2014-08-19 16:00:29 +0530

    Refs #30 Added Moved column in Export Placement page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814054445) do
+ActiveRecord::Schema.define(version: 20140819033025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20140814054445) do
     t.integer  "customer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "moved"
   end
 
   create_table "import_items", force: true do |t|
