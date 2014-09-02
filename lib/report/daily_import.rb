2b1e331ed341489c25b6ef31c200878b5b8c4049
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-09-02 19:15:55 +0530

    Refs #44 Added In Transit Worksheet

---
+++ lib/report/daily_import.rb
@@ -0,0 +1,68 @@
+module Report
+  class DailyImport
+
+    def create(customer)
+      time = DateTime.parse(Time.now.to_s).strftime("%d_%b_%Y")
+      package = Axlsx::Package.new
+      workbook = package.workbook
+      imports = customer.imports.includes(:import_items)
+
+      workbook.styles do |s|
+        heading = s.add_style alignment: {horizontal: :center},
+          b: true, sz: 12, bg_color: "00B0F0", wrap_text: true,
+          :border => {:style => :thin, :color => "00"}
+        center = s.add_style alignment: {vertical: :top, wrap_text: true },
+          :border => {:style => :thin, :color => "00" }
+
+        # 2 worksheets in the workbook
+        { 'In Transit' => nil}.each do |name, status|
+          workbook.add_worksheet(name: name) do |sheet|
+            add_data(imports, sheet, center, heading, status)
+          end
+        end
+      end
+      package.use_shared_strings = true
+
+      package.serialize("#{Rails.root}/tmp/Imports_#{customer.name.tr(" ", "_")}_#{time}.xlsx")
+
+    end
+
+    def add_data(imports, sheet, center, heading, status)
+      sheet.add_row ["Work Order No", "BL Number", "Container Number", "Size",
+                     "Goods Description", "ETA", "Out Of Port Date", "Full remarks",
+                     "Truck Number", "Trailer Number", "Bond Direction", "Bond Number"],
+                  style: heading, height: 40
+      imports.each do |import|
+        import.import_items.each do |item|
+          array = ""
+          out_of_port_date = ""
+          [import,item].each do |entity|
+            entity.audits.collect(&:audited_changes).each do |a|
+              if !a[:status].blank? then
+                array.concat(a[:updated_at].second.to_date.strftime("%d-%b-%Y") + " " +
+                             a[:status].second + "\n")
+                if a[:status].second.eql?("enroute_nairobi")
+                  out_of_port_date = a[:updated_at].second.to_date.strftime("%d-%b-%Y")
+                end
+              end
+            end
+            entity.remarks.nil? ? array : array.concat("Remarks:" + entity.remarks + "\n")
+          end
+
+          sheet.add_row [import.work_order_number, import.bl_number,
+                     item.container_number, import.equipment, import.description,
+                     import.estimate_arrival.to_date.strftime("%d-%b-%Y"), out_of_port_date,
+                     array, item.truck_number, item.bond_direction, item.bond_number],
+                     style: center, height: 180
+
+          array.clear
+
+
+        end
+      end
+
+
+    end
+
+  end
+end
