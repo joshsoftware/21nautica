c77b1a1a532b97295849ab45ccb78d2a66ed0569
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-09-17 16:40:01 +0530

    test case fix for import controller testcase

--- test/controllers/imports_controller_test.rb
+++ test/controllers/imports_controller_test.rb
@@ -6,22 +6,23 @@ class ImportsControllerTest < ActionController::TestCase
     @user = FactoryGirl.create :user
     sign_in @user
     @import = FactoryGirl.create :import
+    @customer = FactoryGirl.create :customer
   end
 
   test "should not save import with duplicate bl_number" do
     assert_no_difference('Import.count') do
-      post :create, import: {bl_number: 'BL1', to: "momabasa", from: "k", 
-        shipping_line: "Maersk", estimate_arrival: "06-10-2014", equipment: "20GP", 
+      post :create, import: {bl_number: 'BL1', to: "momabasa", from: "k",
+        shipping_line: "Maersk", estimate_arrival: "06-10-2014", equipment: "20GP",
         quantity: "1", description: "tfy", import_items_attributes: {"1410153969411" => {container_number: "1"}}}
-    end 
+    end
   end
 
   test "should create new import" do
     assert_difference('Import.count') do
-      post :create, import: {bl_number: 'BL2', to: "momabasa", from: "k", 
-        shipping_line: "Maersk", estimate_arrival: "06-10-2014", equipment: "20GP", 
+      post :create, import: {bl_number: 'BL2', to: "momabasa", from: "k", customer_id: @customer.id,
+        shipping_line: "Maersk", estimate_arrival: "06-10-2014", equipment: "20GP",
         quantity: "1", description: "tfy", import_items_attributes: {"1410153969411" => {container_number: "1"}}}
-    end 
+    end
     assert_redirected_to imports_path
   end
 
@@ -36,7 +37,7 @@ class ImportsControllerTest < ActionController::TestCase
     xhr :post, :update, { id: @import.id,
                     columnName: 'Work Order Number',
                     value: 'WON'
-                    } 
+                    }
     @import.reload
     assert_equal 'WON', @import.work_order_number
   end
@@ -47,7 +48,7 @@ class ImportsControllerTest < ActionController::TestCase
     #assert_select 'form select option', :count => 2
     assert_raises(AASM::InvalidTransition) do
       xhr :post, :updateStatus, import: {status: "ready_to_load", "remarks"=>"okay"}, id: @import.id
-    end 
+    end
     xhr :post, :updateStatus, import: {status: "original_documents_received", "remarks"=>"okay"}, id: @import.id
     xhr :post, :updateStatus, import: {status: "container_discharged", "remarks"=>"okay"}, id: @import.id
     xhr :post, :updateStatus, import: {status: "ready_to_load", "remarks"=>"okay"}, id: @import.id
