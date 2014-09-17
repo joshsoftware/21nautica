a630c2fb0631dd0ddc81d1431f9b63a76986c785
RutujaGhule <rutuja@joshsoftware.com>
2014-09-17 15:55:31 +0530

    Change in factory file to fix circleci load error

--- test/factories/import_item_factory.rb
+++ test/factories/import_item_factory.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do
   
-  factory :import_item1, class: ImportItem do
+  factory :import_item1, class:ImportItem do
   	container_number	'c1'   
   	association				:import
   end
 
-  factory :import_item2, class: ImportItem do
+  factory :import_item2, class:ImportItem do
   	container_number	'c2'  
   	association				:import 
   end
 
-  factory :import_item3, class: ImportItem do
+  factory :import_item3, class:ImportItem do
   	container_number	'c3'   
   	association				:import
   end
