379685482e9ce03c1843b03a295eae608ea477bc
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-10-16 18:18:17 +0530

    added new shipping line Emirates

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,7 +3,7 @@ require 'patch'
 DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
-SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL)
+SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates)
 TRANSPORTERS = %W(Mansons Farsham Panafrica Crown Panafrica\ Logistics Midland\ Hauliers Blue\ Jay)
 # Time in Days
 STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
@@ -12,3 +12,4 @@ STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           arrived_port: 3,
                           document_handed: 1}
 CLEARING_AGENTS = %W(Panafrica EACL)
+
