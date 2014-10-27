ccc0462d3b3c815645d7779a79dacf3b6755bbda
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-10-27 11:28:30 +0530

    added new port of discharge

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,6 @@
 require 'patch'
 
-DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai)
+DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai Jebel\ Ali Mersin)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
 SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates)
