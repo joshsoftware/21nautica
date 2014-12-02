60fc4563969f02ae91e36b332c3df82f831168e0
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-02 15:13:13 +0530

    moved daily report methods from controller to module

---
+++ lib/report/daily_report.rb
@@ -0,0 +1,29 @@
+module Report
+  class DailyReport
+    def daily_report_export
+      customers = []
+      movements = Movement.where.not(status: "container_handed_over_to_KPA")
+      movements.each do |movement|
+        export = Export.find_by id: movement.export_item.export_id
+        customers.push(export.customer)
+      end
+      customers = customers.uniq
+      customers.each do |customer|
+        UserMailer.mail_report(customer,'export').deliver
+      end
+    end
+
+    def daily_report_import
+      customers = []
+      import_items = ImportItem.where.not(status: "delivered").select(:import_id).uniq
+      import_items.each do |item|
+        import = Import.find(item.import_id)
+        customers.push(import.customer)
+      end
+      customers = customers.uniq
+      customers.each do |customer|
+        UserMailer.mail_report(customer,'import').deliver
+      end
+    end
+  end
+end
