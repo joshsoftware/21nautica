b5a8ae88bc49921aa552e9b3d6eb4eb20ee93830
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-04 13:54:58 +0530

    few changes in delta report

--- lib/expense/delta.rb
+++ lib/expense/delta.rb
@@ -1,11 +1,10 @@
 module Expense
-  class Delta
+ class Delta
     def self.generate_report
 
       time = DateTime.parse(Time.now.to_s).strftime("%d_%b_%Y")
       package = Axlsx::Package.new
       workbook = package.workbook
-
       ['Import Expenses', 'Export', 'BL Payment'].each do |name|
         workbook.add_worksheet(name: name) do |sheet|
           add_import_expenses_data(sheet) if name.eql?("Import Expenses")
@@ -17,7 +16,6 @@ module Expense
             pane.x_split = 2
             pane.active_pane = :bottom_right
           end
-
         end
       end
       package.use_shared_strings = true
@@ -36,13 +34,13 @@ module Expense
       import_items.each do |import_item|
         max_height = 0
         import_item.import_expenses.each do |expense|
-          audits = expense.audits.where("updated_at > ?",
-                    (Time.now.utc - 1.days)).order(created_at: :asc).collect(&:audited_changes)
+          audits = find_audits(expense)
           audits.each do |audit|
-            (h[expense.category + "_" + "name"] ||= []).push(audit[:name].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[:name].blank?
-            (h[expense.category + "_" + "amount"] ||= []).push(audit[:amount].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[:amount].blank?
+            ["name", "amount"].each do |field|
+              (h[expense.category + "_" + field] ||= []).push(audit[field.to_sym].second +
+           " at: " + audit[:updated_at].second.to_s) if !audit[field.to_sym].blank?
+            end
+
           end
         end
         h, max_height = format_hash_entries(h, max_height)
@@ -64,8 +62,8 @@ module Expense
       max_height = 0
 
       movements.each do |movement|
-        h, max_height = get_audits_data(movement, max_height,"transporter_name",
-         "transporter_payment", "clearing_agent", "clearing_agent_payment")
+        h, max_height = get_audits_data(movement, max_height,
+                          field_array = ["transporter_name", "transporter_payment", "clearing_agent", "clearing_agent_payment"])
         sheet.add_row [movement.bl_number, movement.container_number,
           h["transporter_name"],h["transporter_payment"], h["clearing_agent"],
           h["clearing_agent_payment"]], height: max_height, alignment: { wrap_text: true}
@@ -82,8 +80,9 @@ module Expense
       max_height = 0
 
       bills_of_lading.each do |bill_of_lading|
-        h, max_height = get_audits_data(bill_of_lading, max_height,"payment_ocean",
-         "cheque_ocean", "payment_clearing", "cheque_clearing")
+        h, max_height = get_audits_data(bill_of_lading, max_height,
+                          field_array = ["payment_ocean", "cheque_ocean",
+                            "payment_clearing", "cheque_clearing"])
         sheet.add_row [bill_of_lading.bl_number,
           Import.where(bl_number: bill_of_lading.bl_number).blank? ? "export" : "import",
           h["payment_ocean"],h["cheque_ocean"], h["payment_clearing"],
@@ -92,19 +91,14 @@ module Expense
       sheet.column_widths 15, 15, 30, 30, 30, 30
     end
 
-    def self.get_audits_data(data_entry, max_height, field1, field2, field3, field4)
+    def self.get_audits_data(data_entry, max_height, field_array)
       h = {}
-      audits = data_entry.audits.where("updated_at > ?",
-       (Time.now.utc - 1.days)).order(created_at: :asc).collect(&:audited_changes)
+      audits = find_audits(data_entry)
       audits.each do |audit|
-        (h[field1] ||= []).push(audit[field1.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field1.to_sym].blank?
-        (h[field2] ||= []).push(audit[field2.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field2.to_sym].blank?
-        (h[field3] ||= []).push(audit[field3.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field3.to_sym].blank?
-        (h[field4] ||= []).push(audit[field4.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field4.to_sym].blank?
+        field_array.each do |field|
+          (h[field] ||= []).push(audit[field.to_sym].second +
+           " at: " + audit[:updated_at].second.to_s) if !audit[field.to_sym].blank?
+        end
       end
       h, max_height = format_hash_entries(h, max_height)
       return h, max_height
@@ -115,6 +109,11 @@ module Expense
       h.each_value{|v| max_height = v.length if v.length > max_height}
       max_height = (max_height * 0.7) if max_height > 50
       return h, max_height
+    end
+
+    def self.find_audits(data_entry)
+      audits = data_entry.audits.where("updated_at > ?",
+       (Time.now.utc - 1.days)).order(created_at: :asc).collect(&:audited_changes)
     end
 
   end
