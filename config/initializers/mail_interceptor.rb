18d354d77902fc59f0dfa9e0440dbead0b708da1
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-08 16:41:49 +0530

    refs #65 #67 removed mail interceptor duplication

---
+++ config/initializers/mail_interceptor.rb
@@ -0,0 +1,6 @@
+class MailInterceptor
+  def self.delivering_email(message)
+    message.to = ['anuja.joshi@joshsoftware.com']
+  end
+end
+ActionMailer::Base.register_interceptor(MailInterceptor) unless Rails.env.production?
--- config/initializers/staging_mail_interceptor.rb
+++
@@ -1,7 +0,0 @@
-class StagingMailInterceptor
-  def self.delivering_email(message)
-    message.to = ['anuja.joshi@joshsoftware.com']
-  end
-end
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.staging?
-
