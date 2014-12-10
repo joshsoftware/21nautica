96335aff6f4a9553194631b9b3c9e8d862cc42af
anuja-joshi <anuaj.joshi@joshsoftware.com>
2014-12-10 13:08:34 +0530

    refs #73 disable Payment Clearing, Cheque Clearing for Export BL

--- app/models/bill_of_lading.rb
+++ app/models/bill_of_lading.rb
@@ -2,4 +2,8 @@ class BillOfLading < ActiveRecord::Base
   validates_uniqueness_of :bl_number
   auditable only: [:payment_ocean, :cheque_ocean,
     :payment_clearing, :cheque_clearing, :updated_at]
+  
+  def is_export_bl?
+    Import.where(bill_of_lading_id: self.id.to_s).blank?
+  end
 end
