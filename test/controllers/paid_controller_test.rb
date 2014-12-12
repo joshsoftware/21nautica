dcf19db0ce463a10b474a657d7bb0b33f8e152d7
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-12 18:11:39 +0530

    added test cases for paid, received controller

--- test/controllers/paid_controller_test.rb
+++ test/controllers/paid_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PaidControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = FactoryGirl.create :user
+    sign_in @user
+  end
+
+  test "should get new" do
+    get :new
+    assert_not_nil assigns(@paid)
+    assert_response :success
+  end
+
+  test "should create paid payment" do
+    assert_difference('Paid.count') do
+      post :create, paid: {amount: 400}
+    end
+    assert_response :redirect
+  end
+
+  test "should not save paid payment without amount" do
+    assert_no_difference('Paid.count') do
+      post :create, paid: {participants_name: "ABC"}
+    end
+    assert_response :success
+  end
+
 end
--- test/controllers/received_controller_test.rb
+++ test/controllers/received_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ReceivedControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = FactoryGirl.create :user
+    sign_in @user
+  end
+
+  test "should get new" do
+    get :new
+    assert_not_nil assigns(@received)
+    assert_response :success
+  end
+
+  test "should create received payment" do
+    assert_difference('Received.count') do
+      post :create, received: {amount: 400}
+    end
+    assert_response :redirect
+  end
+
+  test "should not save received payment without amount" do
+    assert_no_difference('Received.count') do
+      post :create, received: {participants_name: "ABC"}
+    end
+    assert_response :success
+  end
+
 end
