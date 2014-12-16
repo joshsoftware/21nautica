70012a1b2ab5ca28a9ef379f30029baee5bc34c4
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-16 17:22:42 +0530

    refs #73 added migration to remove shipping line from BillOfLading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216100316) do
+ActiveRecord::Schema.define(version: 20141216104508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20141216100316) do
     t.string   "remarks"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "shipping_line"
   end
 
   create_table "customers", force: true do |t|
