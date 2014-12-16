56e1cd39b4be32356778ae6dc0e149961256cb2e
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-16 17:29:51 +0530

    refs #73 added method shipping line to bill_of_lading

--- app/models/bill_of_lading.rb
+++ app/models/bill_of_lading.rb
@@ -1,9 +1,15 @@
 class BillOfLading < ActiveRecord::Base
+  has_one :import
+  has_many :movements
   validates_uniqueness_of :bl_number
   auditable only: [:payment_ocean, :cheque_ocean,
     :payment_clearing, :cheque_clearing, :updated_at]
-  
+
   def is_export_bl?
     Import.where(bill_of_lading_id: self.id.to_s).blank?
+  end
+
+  def shipping_line
+    self.import.try(&:shipping_line) || self.movements.first.try(&:shipping_seal)
   end
 end
