4bf5f03c14247b07888756f93aaefad023e637f6
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-19 13:10:15 +0530

    refs #65 logical changes for generating delta report

--- lib/expense/delta.rb
+++ lib/expense/delta.rb
@@ -1,102 +1,81 @@
 module Expense
   class Delta
     def self.generate_report
-      ReportHelper::add_worksheet_and_data(Delta)
+      time = DateTime.parse(Time.now.to_s).strftime("%d_%b_%Y")
+      package = Axlsx::Package.new
+      workbook = package.workbook
+      ['Import Expenses', 'Export', 'BL Payment'].each do |name|
+        workbook.add_worksheet(name: name)
+      end
+      add_import_expense_data(workbook)
+      fetch_bol_export_audits(workbook)
+      package.use_shared_strings = true
+      package.serialize("#{Rails.root}/tmp/Expense_Delta_#{time}.xlsx")
     end
 
-    def self.add_import_expenses_data(sheet)
+    def self.add_import_expense_data(workbook)
+      sheet = workbook.sheet_by_name("Import Expenses")
       sheet.add_row ["BL Number", "Container Number", "Haulage-Name",
                      "Haulage-Amount", "Empty Name", "Empty Amount",
                      "Final Clearing Name", "Final Clearing Amount",
                      "Demurrage Name", "Demurrage Amount", "ICD name",
                      "ICD amount"]
-      import_items = ImportItem.includes(:import_expenses)
-      h = {}
-      import_items.each do |import_item|
-        max_height = 0
-        import_item.import_expenses.each do |expense|
-          audits = find_audits(expense)
-          audits.each do |audit|
-            ["name", "amount"].each do |field|
-              (h[expense.category + "_" + field] ||= []).push(audit[field.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field.to_sym].blank?
-            end
-
-          end
-        end
-        h, max_height = format_hash_entries(h, max_height)
-        sheet.add_row [import_item.bl_number, import_item.container_number,
-          h["Haulage_name"], h["Haulage_amount"], h["Empty_name"],
-          h["Empty_amount"], h["Final Clearing_name"], h["Final Clearing_amount"],
-          h["Demurrage_name"], h["Demurrage_amount"], h["ICD_name"],
-          h["ICD_amount"]], height: max_height unless h.blank?
-        h.clear
+      audits = Espinita::Audit.where(created_at: 1.day.ago..Time.now.utc, auditable_type: "ImportExpense")
+      audits_hash = {}
+      audits.each do |audit|
+        import_item = audit.auditable.import_item.id
+        category = audit.auditable.category
+        audit.audited_changes.each do |key,value|
+          ((audits_hash[import_item] ||= {}) [category + 
+            "_" + key.to_s] ||= " ").concat("\n #{value.second.to_s}")
+        end 
       end
-      sheet.column_widths 15, 15, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20
+      audits_hash.each do |key, value|
+        import_item = ImportItem.where(id: key).first
+        sheet.add_row [import_item.try(:bl_number), import_item.try(:container_number), 
+          value["Haulage_name"], value["Haulage_amount"], value["Empty_name"], value["Empty_amount"], 
+          value["Final Clearing_name"], value["Final Clearing_amount"], value["Demurrage_amount"], 
+          value["Demurrage_name"], value["ICD_name"], value["ICD_amount"]], height: 30
+      end
     end
 
-    def self.add_export_data(sheet)
+    def self.fetch_bol_export_audits(workbook)
+      audits = Espinita::Audit.where(created_at: 1.day.ago..Time.now.utc, 
+        auditable_type: ["Movement", "BillOfLading"])
+      audits_hash = {}
+      audits.each do |audit|
+        audit.audited_changes.each do |key,value|
+          (((audits_hash[audit.auditable_type] ||= {}) [audit.auditable_id] ||= 
+            {}) [key] ||= "").concat("\n #{value.second.to_s}")
+        end 
+      end
+      add_export_data(workbook.sheet_by_name("Export"),audits_hash["Movement"])
+      add_bl_payments_data(workbook.sheet_by_name("BL Payment"), audits_hash["BillOfLading"])
+    end
+
+    def self.add_export_data(sheet, data)
       sheet.add_row ["BL Number", "Container Number", "Transporter Name",
                      "Transporter Payment", "Clearing Agent Name",
                      "Clearing Agent Payment"]
-      movements = Movement.all
-      max_height = 0
-
-      movements.each do |movement|
-        h, max_height = get_audits_data(movement, max_height,
-                          field_array = ["transporter_name", "transporter_payment", "clearing_agent", "clearing_agent_payment"])
-        sheet.add_row [movement.bl_number, movement.container_number,
-          h["transporter_name"],h["transporter_payment"], h["clearing_agent"],
-          h["clearing_agent_payment"]], height: max_height unless h.blank?
+      data.each do |key, value|
+        movement = Movement.where(id: key).first
+        sheet.add_row [movement.try(:bl_number), movement.try(:container_number), 
+          value[:transporter_name], value[:transporter_payment], value[:clearing_agent], 
+          value[:clearing_agent_payment]], height: 30
       end
-      sheet.column_widths 15, 15, 30, 30, 30, 30
-
     end
 
-    def self.add_bl_payment_data(sheet)
+    def self.add_bl_payments_data(sheet, data)
       sheet.add_row ["BL Number", "Type", "Payment Ocean",
                      "Cheque Ocean", "Payment Clearing",
                      "Cheque Clearing"]
-      bills_of_lading = BillOfLading.all
-      max_height = 0
-
-      bills_of_lading.each do |bill_of_lading|
-        h, max_height = get_audits_data(bill_of_lading, max_height,
-                          field_array = ["payment_ocean", "cheque_ocean",
-                            "payment_clearing", "cheque_clearing"])
-        sheet.add_row [bill_of_lading.bl_number,
-          Import.where(bl_number: bill_of_lading.bl_number).blank? ? "export" : "import",
-          h["payment_ocean"],h["cheque_ocean"], h["payment_clearing"],
-          h["cheque_clearing"]], height: max_height unless h.blank?
+      data.each do |key, value|
+        bol = BillOfLading.where(id: key).first
+        sheet.add_row [bol.bl_number, bol.is_export_bl? ? "Export" : "Import", 
+          value[:payment_ocean], value[:cheque_ocean], value[:payment_clearing], 
+          value[:cheque_clearing]],height: 30
       end
-      sheet.column_widths 15, 15, 30, 30, 30, 30
     end
-
-    def self.get_audits_data(data_entry, max_height, field_array)
-      h = {}
-      audits = find_audits(data_entry)
-      audits.each do |audit|
-        field_array.each do |field|
-          (h[field] ||= []).push(audit[field.to_sym].second +
-           " at: " + audit[:updated_at].second.to_s) if !audit[field.to_sym].blank?
-        end
-      end
-      h, max_height = format_hash_entries(h, max_height)
-      return h, max_height
-    end
-
-    def self.format_hash_entries(h, max_height)
-      h.replace( h.merge(h) {|key, value| value = value.join("\n")} )
-      h.each_value{|v| max_height = v.length if v.length > max_height}
-      max_height = (max_height * 0.7) if max_height > 50
-      return h, max_height
-    end
-
-    def self.find_audits(data_entry)
-      audits = data_entry.audits.where("updated_at > ?",
-       (Time.now.utc - 1.days)).order(created_at: :asc).collect(&:audited_changes)
-    end
-
+ 
   end
-
 end
