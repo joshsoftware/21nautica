0cf15d46e6c81b691c6a061b4605dcfeadd6dda9
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-23 13:13:24 +0530

    refs #81 removed TRANSPORTERS form initalizers

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -4,7 +4,7 @@ DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai Jebel\ Ali Mersin)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
 SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates)
-TRANSPORTERS =  Vendor.pluck(:name)
+#TRANSPORTERS =  Vendor.pluck(:name)
 #%W(Mansons Farsham Panafrica Crown Panafrica\ Logistics Midland\ Hauliers Blue\ Jay)
 # Time in Days
 STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
