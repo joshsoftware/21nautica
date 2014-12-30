d01df3eb4e4866a7e4577012700430249889c72b
Anuja Joshi <joshi_anuja@rocketmail.com>
2014-12-30 20:31:20 +0530

    refs #70 added filtering based on Number of days payments->made

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_date_range
+  	current_date = Date.current
+  	end_date = Date.current.to_s
+  	days_select_box_data = [["All Time", Paid.minimum(:date_of_payment).to_s + "*" + end_date],
+  	  ["Last 30 days", (current_date - 30.days).to_s + "*" + end_date], 
+  	  ["Last 60 days",  (current_date - 60.days).to_s + "*" + end_date], 
+  	  ["Last 90 days", (current_date - 90.days).to_s + "*" + end_date]]
+  end
 end
