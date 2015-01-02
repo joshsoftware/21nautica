26c787c65b74a055a7b7c0edfddc346a0f6a8a90
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-01-02 17:22:26 +0530

    refs #82 added payments -> invoices screen

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
   resources :imports, only: [:new, :create]
   resources :paid, only: [:new, :create, :index]
   resources :received, only: [:new, :create, :index]
+  resources :invoices, only: [:index]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
