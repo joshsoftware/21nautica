67618137f29bae16e5882d9c9c8d313092a03ffb
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-01-11 01:04:59 +0530

    bug fix, display correct under_loding_date of ImportItems

--- app/helpers/movements_helper.rb
+++ app/helpers/movements_helper.rb
@@ -1,11 +1,15 @@
 module MovementsHelper
   def status_updated_at(element)
-    audit = element.audits.select do |k|
-      k[:audited_changes][:status].present? &&
-      !(k[:audited_changes][:status].second.eql?(k[:audited_changes][:status].first)) and
+    if (element.is_a?(ImportItem) && element.under_loading_process?)
+      audit = import_item_loading_date(element)
+    else
+      audit = element.audits.select do |k|
+        k[:audited_changes][:status].present? &&
+        !(k[:audited_changes][:status].second.eql?(k[:audited_changes][:status].first)) and
              k[:audited_changes][:status].second == element.status
+      end
     end
-    audit.first.present? ? audit.first.audited_changes[:updated_at].second : nil 
+    audit.first.present? ? audit.first.audited_changes[:updated_at].second : "" 
   end
 
   def alert(updated_at, element)
@@ -13,4 +17,9 @@ module MovementsHelper
     (Time.now - updated_at) > time 
   end
 
+  def import_item_loading_date(element)
+    element.import.audits.select do |audit_entry|
+      audit_entry[:audited_changes][:status] == ["container_discharged", "ready_to_load"]
+    end
+  end
 end
