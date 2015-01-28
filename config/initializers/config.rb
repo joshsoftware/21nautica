afa8cd350053945af47f5c92ae3ce786f9ef0a2c
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-01-28 11:37:16 +0530

    refs #89 corrected constant name

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -4,8 +4,6 @@ DESTINATION_PORTS = %W(Nhava\ Sheva Mundra Tuticorin Chennai Jebel\ Ali Mersin)
 TYPE = %W(TBL Haulage)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
 SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates)
-#TRANSPORTERS =  Vendor.pluck(:name)
-#%W(Mansons Farsham Panafrica Crown Panafrica\ Logistics Midland\ Hauliers Blue\ Jay)
 # Time in Days
 STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           crossed_malaba_border: 2,
@@ -14,6 +12,6 @@ STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           document_handed: 1}
 CLEARING_AGENTS = %W(Panafrica EACL Agility)
 PAYMENT_MODES = %W(Cheque Cash TT)
-INVOICE_PERTICULARS = %w( Ocean\ Freight Clearing\ Charges ICD Demurrage 
+INVOICE_PARTICULARS = %w( Ocean\ Freight Clearing\ Charges ICD Demurrage 
 	Empty Final\ Clearing Haulage Forest\ Permits Other)
 
