4f809f969504a807ec53b1d61bfe70bd08a338fa
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-01-28 20:46:13 +0530

    added paul to CLEARING_AGENTS

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -12,6 +12,6 @@ STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           order_released: 2,
                           arrived_port: 3,
                           document_handed: 1}
-CLEARING_AGENTS = %W(Panafrica EACL Agility)
+CLEARING_AGENTS = %W(Panafrica EACL Agility Paul)
 PAYMENT_MODES = %W(Cheque Cash TT)
 
