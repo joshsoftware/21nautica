e5bd2c580947cc6f97840d9c6a026af2d66c0509
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-01-29 17:20:26 +0530

    refs #89 changed route to fix status filer search

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
   resources :received, only: [:new, :create, :index]
   resources :invoices, only: [:index, :edit, :update] do
     member do
-      get 'new-additional-invoice'
+      get 'add-additional-invoice' => "invoices#new_additional_invoice"
       post 'additional-invoice'
       get 'download'
       get 'send_invoice'
