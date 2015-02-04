f1c61f8c480d22d6107b78cc6640861fedfdd223
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-02-04 19:57:48 +0530

    removed redundant file

--- config/initializers/development_mail_interceptor.rb
+++
@@ -1,6 +0,0 @@
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.to = ['anuja.joshi@joshsoftware.com']
-  end
-end
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
