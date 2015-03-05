ba0826d8a6db7a838e71950fd3fee0a2de233839
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-03-05 18:02:09 +0530

    adjusted pdf margins

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,12 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  config.default_options = {
+    :encoding=>"UTF-8",
+    :page_size=>"A4",
+    :margin_top=>"0.1in",
+    :margin_right=>"0.1in",
+    :margin_bottom=>"0.1in",
+    :margin_left=>"0.1in",
+    :disable_smart_shrinking=> false
+  }
 end
