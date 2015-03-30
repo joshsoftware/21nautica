4be8502a830eb1cc9bb0cd7f4d82a9383de7e974
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-03-30 10:38:45 +0530

    added additional email-id to invoice mailing list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -36,7 +36,7 @@ class UserMailer < ActionMailer::Base
     attachment_name = File.basename attachment
     attachments[attachment_name] = File.read(attachment)
     subject = "#{invoice.customer_name} // #{invoice.bl_number} // #{invoice.number} // #{invoice.amount}"
-    mail(to: invoice.customer.emails , subject: subject)
+    mail(to: (invoice.customer.emails + ", accounts@21nautica.com") , subject: subject)
     File.delete(attachment)
   end
 
