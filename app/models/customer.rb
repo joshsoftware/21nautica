3794ee9a56e93a223a4564c697225904d060ccb9
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-04-29 11:28:01 +0530

    refs #99 changed customer name validation message

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,7 +18,7 @@ class Customer < ActiveRecord::Base
 
   before_create :assign_emails
 
-  validates_uniqueness_of :name, case_sensitive: false
+  validates_uniqueness_of :name, case_sensitive: false, message: "Customer with same name already exists"
 
   def assign_emails
     self.emails = self.emails + ", accounts@21nautica.com, kaushik@21nautica.com, sachin@21nautica.com, docs@21nautica.com, docs-ug@21nautica.com, ops-ug@21nautica.com"
