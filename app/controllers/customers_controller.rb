7071ace626579a898488b8fa95ca0bba53ba1fb5
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-04-29 11:42:52 +0530

    refs #99 minor changes customer validation error massages

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,7 +6,7 @@ class CustomersController < ApplicationController
 
   def create
     customer = Customer.new(customer_params)
-    customer.save ? @customers = Customer.order(:name).to_a : @errors = customer.errors.full_messages
+    customer.save ? @customers = Customer.order(:name).to_a : @errors = customer.errors.messages.values.flatten
     render 'new'
   end
 
