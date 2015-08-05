b5ccebacc8838268d641fc77ac49d399f6d782e6
Atul Bhosale <atul@joshsoftware.com>
2015-08-05 21:14:46 +0530

    Added Atul's email-id to mail interceptor

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,6 +1,6 @@
 class MailInterceptor
   def self.delivering_email(message)
-    message.to = ['anuja.joshi@joshsoftware.com']
+    message.to = ['anuja.joshi@joshsoftware.com, atul@joshsoftware.com']
   end
 end
 ActionMailer::Base.register_interceptor(MailInterceptor) unless (Rails.env.production? || Rails.env.test?)
