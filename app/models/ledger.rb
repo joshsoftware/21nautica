dd8d738e83a4fab1ae1c40b18d128a114e050919
Paritosh Botre <paritoshbotre@joshsoftware.com>
2015-11-30 14:55:26 +0530

    fix the calculation in payment received

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,6 +1,73 @@
 class Ledger < ActiveRecord::Base
   belongs_to :customer
   belongs_to :voucher, polymorphic: true
+  after_create :update_ledger_if_payment_made, if: :payment?
+  after_create :update_ledger_if_invoice_made, if: :invoice?
+
+  def payment?
+    voucher_type == 'Payment'
+  end
+
+  def invoice?
+    voucher_type == 'Invoice'
+  end
+
+  def update_ledger_if_payment_made
+    #unpaid = Ledger.where(voucher_type: "Payment", customer: self.customer).where("received < amount").order(date: :asc)
+    unpaid = Ledger.where(voucher_type: "Invoice", customer: self.customer).where("received < amount").order(date: :asc)
+    money = self.amount
+    adjusted_ledger_amt = 0
+
+    unpaid.each do |inv|
+      # return if money got over.
+      return if money == 0
+      
+       if money - (inv.amount - inv.received) > 0
+         money -= inv.amount - inv.received
+         adjusted_amt = inv.amount - inv.received
+
+         adjusted_ledger_amt += inv.amount - inv.received
+         inv.update_columns(received: (inv.received + adjusted_amt))
+
+         self.update_columns(received: adjusted_ledger_amt)
+         #inv.received = inv.amount
+       else
+         inv.received = inv.received + money
+         self.update_attribute(:received, inv.received)
+         money = 0
+       end
+
+       inv.save
+    end
+  end
+
+  def update_ledger_if_invoice_made
+    unpaid = Ledger.where(voucher_type: "Payment", customer: self.customer).where("received < amount").order(date: :asc)
+    #unpaid = Ledger.where(voucher_type: "Invoice", customer: self.customer).where("received < amount").order(date: :asc)
+
+    money = self.amount
+    adjusted_ledger_amt = 0
+    unpaid.each do |inv|
+      # return if money got over.
+      return if money == 0
+
+       if money - (inv.amount - inv.received) > 0
+         money -= inv.amount - inv.received
+         adjusted_amt = inv.amount - inv.received
+         adjusted_ledger_amt += inv.amount - inv.received
+         inv.update_columns(received: (inv.received + adjusted_amt))
+
+         self.update_columns(received: adjusted_ledger_amt)
+       else
+         inv_received = inv.received + money
+         inv.update_columns(received: inv_received)
+         self.update_attributes(received: self.amount)
+         money = 0
+       end
+
+       inv.save
+    end
+  end
 
   def as_json(options={})
     super(only: [:voucher_type, :amount, :received, :date], methods: [:bl_number, :invoice_number, :delete_invoice_or_payment])
--- app/models/received.rb
+++ app/models/received.rb
@@ -13,28 +13,6 @@ class Received < Payment
   #  + Find unpaid invoices (Ledgers that have received < amount) sorted in ascending order by date
   #  + Start filling in the ledgers received amounts until the received payment gets exhausted. 
   def update_ledger
-    self.create_ledger(amount: self.amount, customer: self.customer, date: self.date_of_payment)
-    
-    unpaid = Ledger.where(voucher_type: "Invoice", customer: self.customer).where("received < amount").order(date: :asc)
-
-    money = self.amount
-    unpaid.each do |inv|
-      # return if money got over.
-      return if money == 0
-
-       # Expanding 1-liner logic to make the code readable.
-       # (m - (a - r)) > 0 ? (m = (m - (a - r)); r = a) : (r = r + m; m = 0)
-       if money - (inv.amount - inv.received) > 0
-         # If surplus money left
-         money -= inv.amount - inv.received
-         inv.received = inv.amount
-       else
-         # If no surplus money
-         inv.received = inv.received + money
-         money = 0
-       end
-
-       inv.save
-    end
+    self.create_ledger(amount: self.amount, customer: self.customer, date: self.date_of_payment, received: 0)
   end
 end
