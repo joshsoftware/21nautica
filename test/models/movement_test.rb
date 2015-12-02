89764e4a54678682038edb45ea0fdca168c12ec1
Paritosh Botre <paritoshbotre@joshsoftware.com>
2015-12-02 16:50:31 +0530

    Added test cases for movement model

--- test/models/movement_test.rb
+++ test/models/movement_test.rb
@@ -22,7 +22,8 @@ require 'test_helper'
 
 class MovementTest < ActiveSupport::TestCase
   setup do
-    @movement1 = FactoryGirl.create :movement
+    @movement1 = FactoryGirl.create(:movement, :truck_number => 'c1')
+    @bill_of_lading = FactoryGirl.create(:bill_of_lading)
     @export_item = FactoryGirl.create :export_item
     @export = FactoryGirl.create :export
     @customer = FactoryGirl.create :customer
@@ -33,10 +34,10 @@ class MovementTest < ActiveSupport::TestCase
 
   test "should not assign truck which is not free" do
     movement2 = Movement.new
-    movement2.truck_number = "t1"
+    movement2.truck_number = "c1"
     assert_not movement2.save
     assert movement2.errors.messages[:truck_number].include?(
-                      " t1 is not free !")
+                      " c1 is not free !")
   end
 
   test "should not save movement without truck number" do
@@ -49,6 +50,45 @@ class MovementTest < ActiveSupport::TestCase
   test "json should include container field" do
     assert_not @movement1.attributes.has_key?('container_number')
     assert @movement1.as_json.has_key?('container_number')
+  end
+
+  test 'should not move status to container_handed_over_to_KPA' do
+    @movement1.arrived_malaba_border
+    @movement1.crossed_malaba_border
+    @movement1.order_released
+    @movement1.arrived_port
+
+    assert_raises(AASM::InvalidTransition) do
+      @movement1.document_handed
+    end
+  end
+
+  test 'should not create invoice if Bl_number is not present' do
+    @movement1.bill_of_lading = @bill_of_lading
+    @movement1.bl_number = @bill_of_lading.bl_number
+
+    @movement1.arrived_malaba_border
+    @movement1.crossed_malaba_border
+    @movement1.order_released
+    @movement1.arrived_port
+
+    assert true, @movement1.bill_of_lading.invoices.blank?
+  end
+
+  test 'should create invoice for TBL Export type' do
+    @movement1.movement_type = 'TBL'
+    @movement1.bill_of_lading = @bill_of_lading
+    @movement1.bl_number = @bill_of_lading.bl_number
+    @movement1.save
+
+
+    @movement1.arrived_malaba_border
+    @movement1.crossed_malaba_border
+    @movement1.order_released
+    @movement1.arrived_port
+    @movement1.document_handed
+
+    assert true, @movement1.bill_of_lading.invoices.present?
   end
 
 end
