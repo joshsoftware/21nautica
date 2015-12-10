ac65d746e5f433667481d94e4d8d68e0cd27ac6a
Paritosh Botre <paritoshbotre@joshsoftware.com>
2015-12-10 21:11:17 +0530

    Remove invitiable from users through migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210062252) do
+ActiveRecord::Schema.define(version: 20151210153330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,7 +292,7 @@ ActiveRecord::Schema.define(version: 20151210062252) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
