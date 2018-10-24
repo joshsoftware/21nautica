f4191e4df78e860aaa244fd00e7ea66d5755f960
Paritosh Botre <paritoshbotre@joshsoftware.com>
2018-10-24 12:27:02 +0530

    Added helper method for ug host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :is_rfs_host?
+  helper_method :is_rfs_host?, :is_ug_host?
   before_action :authenticate_user!
 
 
