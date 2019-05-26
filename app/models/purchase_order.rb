f6232e43b198d64eb7109a6396b1ba73ecf72c06
Paritosh Botre <paritoshbotre@joshsoftware.com>
2019-05-26 12:20:11 +0530

    Added validation and set default date

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,4 +3,5 @@ class PurchaseOrder < ActiveRecord::Base
   has_many :purchase_order_items
 
   accepts_nested_attributes_for :purchase_order_items, allow_destroy: true
+  validates_presence_of :number, :date, :vendor_id
 end
