d7f879125ec67bf0206f7a018fa932de532a0851
prashant <prashantbangar03@gmail.com>
2019-09-16 20:28:59 +0530

    Removed unwanted files

--- db/migrate/20190909090927_create_expense_heads.rb
+++ db/migrate/20190909090927_create_expense_heads.rb
@@ -3,6 +3,7 @@ class CreateExpenseHeads < ActiveRecord::Migration
     create_table :expense_heads do |t|
       t.string :name
       t.boolean :is_related_to_truck
+      t.boolean :is_active, :default => true 
       t.timestamps
     end
   end
--- db/migrate/20190909115634_rename_is_truck_to_is_related_to_truck.rb
+++
@@ -1,5 +0,0 @@
-class RenameIsTruckToIsRelatedToTruck < ActiveRecord::Migration
-  def change
-    rename_column :expense_heads, :is_truck, :is_related_to_truck
-  end
-end
