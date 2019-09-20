13ca5636846fac0d861a03863a47efe413a77f60
prashant <prashantbangar03@gmail.com>
2019-09-20 12:17:55 +0530

    removed date from controller and added in model

--- app/controllers/petty_cashes_controller.rb
+++ app/controllers/petty_cashes_controller.rb
@@ -1,20 +1,18 @@
+# Petty Cash Mangement Controller
 class PettyCashesController < ApplicationController
-  before_action :find_petty_cash_id, only: [:edit, :update, :show, :destory]
-  before_action :update_available_balance, only: [:edit, :update, :create]
-  before_action :add_current_date, only: [:edit, :update, :create]
+  before_action :update_available_balance, only: %i[create]
   def index
     @petty_cash = PettyCash.all
   end
 
   def new
     @petty_cash = PettyCash.new
-    @expense_heads = ExpenseHead.active.map{ |expense_head| [expense_head.name, expense_head.id, {'data-is_related_to_truck'=>expense_head.is_related_to_truck}] }
+    @expense_heads = ExpenseHead.active.map { |expense_head| [expense_head.name, expense_head.id, { 'data-is_related_to_truck' => expense_head.is_related_to_truck }] }
     @trucks = Truck.all.map { |truck| [truck.reg_number, truck.id] }
-    @date = DateTime.now.strftime("%d/%m/%Y")
   end
 
   def create
-    @petty_cash = PettyCash.new(petty_cash_params)
+    @petty_cash = current_user.petty_cashes.new(petty_cash_params)
     if @petty_cash.save
       flash[:notice] = 'Petty Cash saved '
       redirect_to :petty_cashes
@@ -24,20 +22,22 @@ class PettyCashesController < ApplicationController
   end
 
   private
+
   def petty_cash_params
-    params.require(:petty_cash).permit(:date, :created_by_id, :transaction_amount, :transaction_type, :description, :expense_head_id, :truck_id, :available_balance)
+    params.require(:petty_cash).permit(
+      :transaction_amount, :transaction_type,
+      :description, :expense_head_id, :truck_id, :available_balance
+    )
   end
 
   def update_available_balance
-    last_available_balance = PettyCash.count == 0 ? 0.00 : PettyCash.last.available_balance
-    if params[:petty_cash][:transaction_type] === "Deposit"
-      params[:petty_cash][:available_balance] = last_available_balance + params[:petty_cash][:transaction_amount].to_f
+    available_balance =
+      PettyCash.count.zero? ? 0.00 : PettyCash.last.available_balance
+    transaction_amount = params[:petty_cash][:transaction_amount].to_f
+    if params[:petty_cash][:transaction_type].eql?('Deposit')
+      params[:petty_cash][:available_balance] = available_balance + transaction_amount
     else
-      params[:petty_cash][:available_balance] = last_available_balance - params[:petty_cash][:transaction_amount].to_f
+      params[:petty_cash][:available_balance] = available_balance - transaction_amount
     end
-  end
-
-  def add_current_date
-    params[:petty_cash][:date] = DateTime.now.strftime("%d/%m/%Y")
   end
 end
