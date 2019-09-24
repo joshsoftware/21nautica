032b449ce1d634394494b3ab5bc1fa0edb51de4f
prashant <prashantbangar03@gmail.com>
2019-09-24 11:10:53 +0530

    Rename remove card

--- app/models/repair_head.rb
+++ app/models/repair_head.rb
@@ -1,5 +1,5 @@
 class RepairHead < ActiveRecord::Base
  validates :name, presence: true
- has_and_belongs_to_many :job_card_details
+#  has_and_belongs_to_many :job_card_details
  scope :active, -> { where(:is_active => true)}
 end
