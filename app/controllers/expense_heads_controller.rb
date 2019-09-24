eefb715b49d2ee89c06947732581059dec425af4
prashant <prashantbangar03@gmail.com>
2019-09-24 11:14:09 +0530

    renamed Find expense headd by it to set expense head

--- app/controllers/expense_heads_controller.rb
+++ app/controllers/expense_heads_controller.rb
@@ -1,6 +1,6 @@
 # Expense Head Class
 class ExpenseHeadsController < ApplicationController
-  before_action :find_expense_head_id, only: %i[edit update]
+  before_action :set_expense_head, only: %i[edit update]
   def index
     @expense_heads = ExpenseHead.all.paginate(page: params[:page], per_page: 20)
   end
@@ -30,7 +30,7 @@ class ExpenseHeadsController < ApplicationController
 
   private
 
-  def find_expense_head_id
+  def set_expense_head
     @expense_head = ExpenseHead.find_by(id: params[:id])
   end
 
