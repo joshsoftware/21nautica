b1123a466d9c2dbfcf6299006d4b56ba1c4ee821
prashant <prashantbangar03@gmail.com>
2019-10-10 17:19:22 +0530

    Added new column and make-model model into truck master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191006075952) do
+ActiveRecord::Schema.define(version: 20191010102807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -283,6 +283,12 @@ ActiveRecord::Schema.define(version: 20191006075952) do
 
   add_index "location_dates", ["truck_id"], name: "index_location_dates_on_truck_id", using: :btree
 
+  create_table "make_models", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "mechanics", force: true do |t|
     t.string   "name"
     t.date     "date_of_employment"
@@ -503,7 +509,14 @@ ActiveRecord::Schema.define(version: 20191006075952) do
     t.string   "status"
     t.string   "location"
     t.integer  "current_import_item_id"
+    t.integer  "fuel_capacity"
+    t.string   "trailer_reg_number"
+    t.decimal  "insurance_premium_amt_yearly",            precision: 10, scale: 2
+    t.string   "driver_name",                  limit: 50
+    t.integer  "make_model_id"
   end
+
+  add_index "trucks", ["make_model_id"], name: "index_trucks_on_make_model_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
