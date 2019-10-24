cc95559ab1d0eb29746f6e5f34712741278927fa
prashant <prashantbangar03@gmail.com>
2019-10-24 16:32:04 +0530

    added condition to drop table

--- db/migrate/20191024100618_drop_transport_manager_table.rb
+++ db/migrate/20191024100618_drop_transport_manager_table.rb
@@ -1,5 +1,5 @@
 class DropTransportManagerTable < ActiveRecord::Migration
   def change
-    drop_table :transport_manager_cashes if TransportManagerCash.present?
+    drop_table :transport_manager_cashes,  if_exists: true
   end
 end
