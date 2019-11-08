14d1bd106d0c81fe401b78aa7090ea29258c5187
Kiran <kiran.mahale@joshsoftware.com>
2019-11-08 12:58:03 +0530

    Adding spare part history

--- app/controllers/spare_parts_controller.rb
+++ app/controllers/spare_parts_controller.rb
@@ -35,6 +35,31 @@ class SparePartsController < ApplicationController
     render json: category.spare_part_categories.pluck(:name, :id)
   end
 
+  def history
+    json_data = []
+    if params[:history_type] == "po"
+      po_history = PurchaseOrderItem.select("purchase_order_items.created_at,
+                    purchase_order_items.spare_part_id, purchase_order_items.quantity,
+                    purchase_order_items.price, suppliers.name supplier_name,
+                    purchase_order_items.purchase_order_id")
+                    .joins(:purchase_order => [:supplier])
+                    .where("purchase_order_items.created_at > ? AND purchase_order_items.spare_part_id= ?", Time.zone.now - 3.months, params[:spare_part_id])
+      data = po_history.offset(params[:start]).limit(params[:length] || 10)
+      json_data = {"data" => data, "recordsTotal" => po_history.count("purchase_order_items.id"), "recordsFiltered" => po_history.count("purchase_order_items.id") }
+    elsif params[:history_type] == "req"
+      req_history = ReqPart.select(:created_at, :price, :quantity)
+                    .where("created_at >= ? AND req_parts.spare_part_id=?",
+                    Time.zone.now - 3.months, params[:spare_part_id])
+      data = req_history.offset(params[:start]).limit(params[:length] || 10)
+      json_data = {:data => data, "recordsTotal" => req_history.count("req_parts.id"), "recordsFiltered" => req_history.count("req_parts.id") }
+    end
+    
+    respond_to do |format|
+      format.html{}
+      format.json { render json: json_data }
+    end    
+  end
+
   private
 
   def spare_part_params
