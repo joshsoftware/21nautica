ee55ba501f9c96822c5e28f7e534e39c2a1b713f
Kiran <kiran.mahale@joshsoftware.com>
2019-12-11 15:00:53 +0530

    Updated the validations for purcahse order item

--- app/models/purchase_order_item.rb
+++ app/models/purchase_order_item.rb
@@ -3,6 +3,8 @@ class PurchaseOrderItem < ActiveRecord::Base
   belongs_to :spare_part
   belongs_to :purchase_order
 
+  validates_presence_of :price, :quantity
+
   after_create :adjust_ledger
   after_destroy :adjust_ledger
 
