ffaa2682175af8add6a02b5042296d9764f6ea73
Kiran <kiran.mahale@joshsoftware.com>
2019-12-11 16:04:45 +0530

    changed the condition for adjusting the spare part ledger

--- app/models/req_part.rb
+++ app/models/req_part.rb
@@ -1,4 +1,5 @@
 class ReqPart < ActiveRecord::Base
+  attr_accessor :is_adjustment
   belongs_to :spare_part
   belongs_to :mechanic
   belongs_to :req_sheet
@@ -6,6 +7,7 @@ class ReqPart < ActiveRecord::Base
   after_destroy :adjust_ledger
 
   validates_presence_of :mechanic_id, :spare_part_id, :price, :quantity
+  validates :price, :quantity, numericality: { greater_than: 0 } , unless: "is_adjustment.present?"
 
   def adjust_ledger
     SparePartLedger.adjust_whole_ledger(self.spare_part_id)
