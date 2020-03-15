b1cbf0dc2bb1d6ded272cb47cae4a7843508aa71
Kiran <kiran.mahale@joshsoftware.com>
2020-03-15 20:09:36 +0530

    Adding petty cash nbo

--- app/helpers/petty_cashes_helper.rb
+++ app/helpers/petty_cashes_helper.rb
@@ -1,3 +1,16 @@
 module PettyCashesHelper
   include ActionView::Helpers::NumberHelper
+
+  def for_account # For choosing petty cash account type
+    url_array = request.original_url.split(/[\/,?]/)
+    if url_array.include?('petty_cashes')
+      return 'Cash'
+    elsif url_array.include?('mpesaes')
+      return 'Bank'
+    elsif url_array.include?('petty_cash_nbos')
+      return 'NBO'
+    else
+      return ''
+    end
+  end
 end
