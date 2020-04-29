2524e6cc03ce0b0356eaca1c57263d7be7e3a417
Kiran <kiran.mahale@joshsoftware.com>
2020-04-29 23:19:29 +0530

    Adding payment reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,9 @@
 class ReportsController < ApplicationController
   
-  def vendor_invoice
+  def vendor_invoices
   end
 
-  def customer_invoice
+  def customer_invoices
     
   end
 
@@ -21,6 +21,15 @@ class ReportsController < ApplicationController
   end
 
   def payments_received
-    
+    @customers = Customer.order(:name).pluck(:name, :id)
+    customer_id = params[:customer_id]
+    @receiveds = (params[:customer_id].present? && params[:customer_id] != 'all') ? Received.where(customer_id: customer_id) : Received.all
+    if !params[:daterange].blank?
+      @receiveds = @receiveds.where(:date_of_payment => Date.parse(params[:daterange].split("-")[0])..Date.parse(params[:daterange].split("-")[1] ))
+    end
+    respond_to do |format|
+      format.html{}
+      format.json { render json: { :data => @receiveds.order(date_of_payment: :desc).offset(params[:start]).limit(params[:length] || 10).map(&:report_json), "recordsTotal" => @receiveds.count, "recordsFiltered" => @receiveds.count } }
+    end
   end
 end
