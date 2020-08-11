cdab66cbc8d0eb70faa2763ebd5674ee3834049c
Kaiwalya <kaiwalya.pataskar@joshsoftware.com>
2020-08-11 18:15:21 +0530

    Fix: External remarks order & latest review on import_tracking

--- app/models/remark.rb
+++ app/models/remark.rb
@@ -5,7 +5,7 @@ class Remark < ActiveRecord::Base
   validates_presence_of :desc, :category
   enum category: ["internal", "external"]
   scope :internal, -> { where(category: 0).order(created_at: :desc) }
-  scope :external, -> { where(category: 1).order(created_at: :desc) }
+  scope :external, -> { where(category: 1).order(created_at: :asc) }
   before_create :add_date
 
   def add_date
