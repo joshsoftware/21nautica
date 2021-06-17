e71b9c971ef8fec3bc55efef73803609fd456a15
Vishwajeetsingh Desurkar <32262119+Selectus2@users.noreply.github.com>
2021-06-18 01:04:28 +0530

    lose cargo changes merged (#195) (#196)

---
+++ app/helpers/lose_cargo_imports_helper.rb
@@ -0,0 +1,2 @@
+module LoseCargoImportsHelper
+end
--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -5,11 +5,13 @@ TYPE = %W(TBL Haulage)
 CURRENCY = %w(USD UGX)
 EQUIPMENT_TYPE = %W(20GP 40GP 40OT 40FR 20OT)
 ROLE = %W(Admin Staff Yard Operations Accounts Transport)
+ORDER_TYPE = %W(containerized Lose)
+REMOVE_ALLOWED_STATUSES = %W(ready_to_load truck_allocated under_loading_process)
 SALES_REP_NAMES = ['Kaushik', 'Rajan']
-EMAILS_DEFAULTS = ENV['EMAILS_DEFAULTS'] 
+EMAILS_DEFAULTS = ENV['EMAILS_DEFAULTS']
 TRUCK_REG_NUMBER = ['Co-Loaded Truck', '3rd Party Truck']
 
-#SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates MSC COSCO 
+#SHIPPING_LINE = %W(CMA\ CGM Maersk  Evergreen Safmarine PIL Emirates MSC COSCO
 #  NYK\ line WECLINES)
 # Time in Days
 STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
@@ -18,7 +20,7 @@ STATUS_CHANGE_DURATION = {arrived_malaba_border: 2,
                           arrived_port: 3,
                           document_handed: 1}
 PAYMENT_MODES = %W(Cheque Cash TT Credit\ Note)
-#INVOICE_PARTICULARS = %w( Ocean\ Freight Clearing\ Charges ICD Demurrage 
+#INVOICE_PARTICULARS = %w( Ocean\ Freight Clearing\ Charges ICD Demurrage
 #	Empty Final\ Clearing Haulage Forest\ Permits Port\ Storage Other)
 INVOICE_PARTICULARS = ["Ocean Freight", "Agency Fee", "THC as per Line", "Container Demurrage", "Port Charges as per KPA", "Final Clearing", "Transport Charges", "Forest Permits", "Port Storage", "Other"]
 PORTS = {"Dar Es Salaam" => ["Kigali", "Lusaka", "Goma"], "Mombasa" => ["Kampala", "Kigali"]}
@@ -38,7 +40,7 @@ ITEM_FOR = { 'transporter'=> ['container'] , 'icd'=> ['container'] , 'shipping_l
 CHARGES_CLASSIFICATION = {'container' =>["Haulage","Empty Return","Truck Detention","Local Shunting",
                                          "ICD Charges", "Border Clearing Expense", "Other charges", 'THC',
                                          'Port Charges', 'Port Storage', 'Others/Misc' ],
-                      'bl' => ['Haulage', "Ocean Freight", "Container Demurrage", "Port Charges", "Shipping Line Charges", "Port Storage", 
+                      'bl' => ['Haulage', "Ocean Freight", "Container Demurrage", "Port Charges", "Shipping Line Charges", "Port Storage",
                                "VAT", "Final Clearing", "Others", "Agency Fee", "Ocean Freight", "Container Demurrage", 'THC',
                                'Port Charges', 'Port Storage', 'Others/Misc']
                         }
